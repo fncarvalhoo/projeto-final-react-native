d1a091c567fa6b55e5cd06c57097392f5e7d8404
guioalmeida <gui.oalmeida2508@gmail.com>
2022-11-21 23:56:55 -0300

    Criando aba Cliente

--- App.tsx
+++ App.tsx
@@ -1,25 +1,12 @@
 import { Home } from "../projeto-final-react-native/src/Screen/Home/index";
-import { Cadastro } from "./src/Screen/Cadastro";
 import { Login } from "./src/Screen/Login";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {Cliente} from "./src/Screen/Cliente"
 
-const Stack = createNativeStackNavigator();
-export default function App() {
+ function App() {
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <Cliente />
+  )  
 }
+
+export default App
