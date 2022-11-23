039c60968fcfb21c62d9dc319f64447386c657cf
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-11-23 12:53:17 -0300

    Correção conflito api

--- src/Screen/Home/index.tsx
+++ src/Screen/Home/index.tsx
@@ -10,9 +10,11 @@ import {
   FlatList,
 } from "react-native";
 import logoNav from "../../Assets/LogoNav.png";
-import { getProduto } from "../../Services/api/api";
 import { styles } from "./styled";
-import { getProduto, listaProdutos } from "../../Services/repository/produtoRepository"
+import {
+  getProduto,
+  listaProdutos,
+} from "../../Services/repository/produtoRepository";
 import { textChangeRangeIsUnchanged } from "typescript";
 
 export const Home = () => {
@@ -20,11 +22,14 @@ export const Home = () => {
   const [listaProdutos, setlistaProdutos] = useState<listaProdutos[]>([]);
 
   useEffect(() => {
-    getProduto().then((res) => {
-      setlistaProdutos(res.data)
-    }).catch((err) => {
-      console.log(err)
-    }).finally(() => setCarregando(false))
+    getProduto()
+      .then((res) => {
+        setlistaProdutos(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setCarregando(false));
   }, []);
 
   return (
@@ -34,37 +39,36 @@ export const Home = () => {
         hidden={false}
         backgroundColor="black"
       />
-      <View >
-
+      <View>
         <View style={styles.nav}>
           <TouchableOpacity style={styles.button}>
             <Text style={styles.textButton}>LOGIN</Text>
           </TouchableOpacity>
         </View>
         <Image source={logoNav} style={styles.logo} />
-        {carregando ?
-          <ActivityIndicator
-            size={"large"}
-          />
-          :
-          <FlatList 
+        {carregando ? (
+          <ActivityIndicator size={"large"} />
+        ) : (
+          <FlatList
             data={listaProdutos}
             numColumns={2}
             renderItem={({ item }) => {
               return (
                 <View style={styles.container}>
-                    <View style={styles.card}>
-                      <Image style={styles.imgCard} source={{ uri: item.fotoLink }} />
-                      <Text style={styles.title}>{item.nome}</Text>
-                      {/* <Text style={styles.title}>{item.descricao}</Text> */}
-                      <Text style={styles.price}> R${item.valor},00</Text>
-                    </View>
+                  <View style={styles.card}>
+                    <Image
+                      style={styles.imgCard}
+                      source={{ uri: item.fotoLink }}
+                    />
+                    <Text style={styles.title}>{item.nome}</Text>
+                    {/* <Text style={styles.title}>{item.descricao}</Text> */}
+                    <Text style={styles.price}> R${item.valor},00</Text>
+                  </View>
                 </View>
-              )
-            }
-            }
+              );
+            }}
           />
-        }
+        )}
       </View>
     </View>
   );
--- src/Services/api/api.ts
+++ src/Services/api/api.ts
@@ -1,35 +1,8 @@
 import axios, { AxiosResponse } from "axios";
 
-const produtos = axios.create({
+export const produtos = axios.create({
   baseURL: "https://21b8-138-117-220-127.sa.ngrok.io/",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-interface listaProdutos {
-  id: string;
-  r;
-  nome: string;
-  descricao: string;
-  qtdEstoque: string;
-  valor: string;
-  idCategoria: string;
-  nomeCategoria: string;
-  idFuncionario: string;
-  nomeFuncionario: string;
-  dataFabricacao: string;
-  fotoLink: string;
-}
-
-interface getProdutoRespostaProps {
-  results: listaProdutos[];
-}
-
-export function getProduto(): Promise<
-  AxiosResponse<getProdutoRespostaProps, any>
-> {
-  let url = `produto/listaProdutos`;
-
-  return produtos.get(url);
-}
