e4780d08b66a5dc3a04f505d1f150c5f9327f446
pedrowiskutzki <wiskutzki@gmail.com>
2022-11-23 01:10:59 -0300

    feat(Correct):Correct

---
+++ src/Services/api.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from "axios";
+
+    export const produtos = axios.create({
+    baseURL: "http://192.168.1.19:8080/",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+
